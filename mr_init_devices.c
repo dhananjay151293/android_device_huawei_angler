07b87b86c2a2e737ccd1ebd3adfc6d189ec21fd5
dhananjay151293 <dhananjay151293@gmail.com>
2016-02-07 18:47:02 +0530

    Add mr_init_devices.c
    Add file required by MR to init device framebuffer, mmc blocks, and usb access.

---
+++ mr_init_devices.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+
+// These are paths to folders in /sys which contain "uevent" file
+// need to init this device.
+// MultiROM needs to init framebuffer, mmc blocks, input devices,
+// some ADB-related stuff and USB drives, if OTG is supported
+// You can use * at the end to init this folder and all its subfolders
+const char *mr_init_devices[] =
+{
+    "/sys/class/graphics/fb0",
+    "/sys/class/graphics/fb1",
+
+    "/sys/block/mmcblk0",
+    "/sys/devices/soc.0",
+    "/sys/devices/soc.0/f9824900.sdhci",
+    "/sys/devices/soc.0/f9824900.sdhci/mmc_host/mmc0",
+    "/sys/devices/soc.0/f9824900.sdhci/mmc_host/mmc0/mmc0:0001",
+    "/sys/devices/soc.0/f9824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0",
+    "/sys/devices/soc.0/f9824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p34", // boot
+    "/sys/devices/soc.0/f9824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p38", // cache
+    "/sys/devices/soc.0/f9824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p1", // modem
+    "/sys/devices/soc.0/f9824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p32", // persist
+    "/sys/devices/soc.0/f9824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p43", // system
+    "/sys/devices/soc.0/f9824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p44", // userdata
+    "/sys/devices/soc.0/f9824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p33", // metadata
+    "/sys/devices/soc.0/f9824900.sdhci/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p37", // vendor
+    "/sys/bus/mmc",
+    "/sys/bus/mmc/drivers/mmcblk",
+    "/sys/module/mmc_core",
+    "/sys/module/mmcblk",
+
+    "/sys/devices/virtual/input*",
+    "/sys/devices/virtual/misc/uinput",
+    "/sys/devices/f9924000.i2c/i2c-2/input*",
+
+    // for adb
+    "/sys/devices/virtual/tty/ptmx",
+    "/sys/devices/virtual/misc/android_adb",
+    "/sys/bus/usb",
+
+    // Encryption
+    "/sys/devices/virtual/misc/device-mapper",
+    "/sys/devices/virtual/misc/ion",
+    "/sys/devices/virtual/qseecom/qseecom",
+
+    NULL
+};
